eb787dd7461eb8556bddbb99432d6e0546f9f382
jslee <qhrgldi1@gmail.com>
2020-05-13 14:51:55 +0900

    2.15 Saving to CSV

--- save.py
+++ save.py
@@ -1,4 +1,11 @@
 import csv
 
 def save_to_file(jobs):
+    file = open("jobs.csv", mode="w", newline="" ,encoding='UTF-8') # 쓰기 모드
+    writer = csv.writer(file)
+    # csv의 첫 줄(row) 작성
+    writer.writerow(["title", "company", "location", "link"])
+    
+    for job in jobs:
+        writer.writerow(list(job.values()))
     return
